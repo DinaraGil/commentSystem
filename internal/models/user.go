d0f3698cfcd6cfe742585eb1d8a64e52cf973c08
dinara <dinaragig03@gmail.com>
2026-03-30 23:22:46 +0300

    feat: add GraphQl autogenerated code

---
+++ internal/models/user.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"fmt"
+	"io"
+	"strconv"
+
+	"github.com/99designs/gqlgen/graphql"
+)
+
+type User struct {
+	ID       int    `json:"id" db:"person_id"`
+	UserName string `json:"username" db:"username"`
+}
+
+type ID int
+
+func MarshalID(id int) graphql.Marshaler {
+	return graphql.WriterFunc(func(w io.Writer) {
+		io.WriteString(w, strconv.Quote(fmt.Sprintf("%d", id)))
+	})
+}
+
+func UnmarshalID(v interface{}) (int, error) {
+	id, ok := v.(string)
+	if !ok {
+		return 0, fmt.Errorf("ids must be strings")
+	}
+	i, e := strconv.Atoi(id)
+	return int(i), e
+}
--- internal/sql/init.sql
+++ internal/sql/init.sql
@@ -1,24 +1,25 @@
-CREATE TABLE IF NOT EXISTS "user" (
-    user_id SERIAL PRIMARY KEY
+CREATE TABLE IF NOT EXISTS person (
+    person_id SERIAL PRIMARY KEY,
+    username VARCHAR(32) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS post (
     post_id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
+    person_id INTEGER NOT NULL,
     content TEXT NOT NULL,
     allow_comment BOOLEAN DEFAULT true,
     created_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY (user_id) REFERENCES "user" (user_id)
+    FOREIGN KEY (person_id) REFERENCES person (person_id)
 );
 
 CREATE TABLE IF NOT EXISTS comment (
     comment_id SERIAL PRIMARY KEY,
     reply_comment_id INTEGER,
     post_id INTEGER,
-    user_id INTEGER,
+    person_id INTEGER,
     content VARCHAR(2000) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY (user_id) REFERENCES "user" (user_id),
+    FOREIGN KEY (person_id) REFERENCES person (person_id),
     FOREIGN KEY (post_id) REFERENCES post (post_id),
     FOREIGN KEY (reply_comment_id) REFERENCES comment (comment_id)
 );
